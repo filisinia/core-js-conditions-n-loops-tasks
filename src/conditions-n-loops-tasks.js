724cdac863e119bd25d88dfb45e4d5bbafe0e276
Mariia Petrenko <filisinia@gmail.com>
2024-01-02 17:07:50 +0200

    feat: solve isPalindrome

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -223,8 +223,15 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let res;
+
+  for (let i = 0; i < str.length / 2; i += 1) {
+    res = str[i] === str[str.length - i - 1];
+    if (!res) break;
+  }
+
+  return res;
 }
 
 /**
