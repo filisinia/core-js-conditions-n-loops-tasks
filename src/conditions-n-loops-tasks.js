2cabbd89e6e04caa6c3989d9b19760486024664f
Mariia Petrenko <filisinia@gmail.com>
2024-01-07 14:17:49 +0200

    feat: solve rotateMatrix

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -401,8 +401,21 @@ function getSpiralMatrix(size) {
  *    [7, 8, 9]         [9, 6, 3]
  *  ]                 ]
  */
-function rotateMatrix(/* matrix */) {
-  throw new Error('Not implemented');
+function rotateMatrix(matrix) {
+  const res = matrix;
+  const arr = [];
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    arr[i] = [...matrix[i]];
+  }
+
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[0].length; j += 1) {
+      res[j][matrix.length - 1 - i] = arr[i][j];
+    }
+  }
+
+  return res;
 }
 
 /**
